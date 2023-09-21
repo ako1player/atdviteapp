896c56d4bad326d92c9ce593b411247b39d64dc8
Adrian Garcia Rios <adrigar10@gmail.com>
2023-09-21 11:01:48 -0400

    added no matches found

--- src/meal/Meal.tsx
+++ src/meal/Meal.tsx
@@ -5,11 +5,16 @@ import MealCard from "./MealCard";
 const Meal = () => {
   const [meal, setMeal] = useState([]);
   const [query, setQuery] = useState("");
+  const [error, setError] = useState("");
 
   const getMeal = useCallback(async () => {
     const res = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${query}`);
     const data = await res.json();
-    setMeal(data.meals);
+    if(data.meals !== null){
+      setMeal(data.meals);
+    } else{
+      setError("No matches for this ingredient")
+    }
   }, [query]);
 
   // useEffect(() => {
@@ -22,12 +27,11 @@ const Meal = () => {
       <div>
         <Search query={query} onQueryChange={(myQuery:any) => setQuery(myQuery)}/>
         <button onClick={getMeal}>Search</button>
+        <p>{error}</p>
       </div>
       <div className="flex flex-wrap">
-      {meal.map((m:any,idx) =>(
-        
+      {meal && meal.map((m:any,idx) =>(
           <MealCard name={m.strMeal} id={m.idMeal} thumbnail={m.strMealThumb} key={idx}/>
-        
       ))}
       </div>
     </main>
